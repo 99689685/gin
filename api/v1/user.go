dfc1ffeeeae41715a3802f170bae2ff6b658b0aa
井科伟 <jeckwell@126.com>
2023-03-05 20:09:33 +0800

    user 模块

--- api/v1/user.go
+++ api/v1/user.go
@@ -1,1 +1,60 @@
 package v1
+
+import (
+	"ginweb/model"
+	"ginweb/utils/errmsg"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+var code int
+
+// QueryUser 查询 用户是否存在
+func QueryUser() {
+
+}
+
+// AddUser 添加用户
+func AddUser(c *gin.Context) {
+	var data model.User
+	_ = c.ShouldBindJSON(&data)
+	code = model.CheckUser(data.Username)
+	if code == errmsg.SUCCESS {
+		model.CreateUser(&data)
+	}
+	if code == errmsg.ErrorUsernameUsed {
+		code = errmsg.ErrorUsernameUsed
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
+
+// 查询单个用户
+
+// QueryUsers 查询用户列表
+func QueryUsers(c *gin.Context) {
+	pageSize, _ := strconv.Atoi(c.Query("pagesize"))
+	pageNum, _ := strconv.Atoi(c.Query("pagenum"))
+	username := c.Query("username")
+	code = errmsg.SUCCESS
+	data, _ := model.GetUsers(username, pageSize, pageNum)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
+
+// EditUser 修改用户
+func EditUser(c *gin.Context) {
+
+}
+
+// DeleteUser 删除用户
+func DeleteUser(c *gin.Context) {
+
+}
--- model/User.go
+++ model/User.go
@@ -1,11 +1,66 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"encoding/base64"
+	"ginweb/utils/errmsg"
+	"golang.org/x/crypto/scrypt"
+	"gorm.io/gorm"
+	"log"
+)
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"type:varchar(20);not null" json:"username"`
-	Password string `gorm:"type:varchar(20);not null" json:"password"`
-	Role     int    `gorm:"type:init" json:"role"`
-	Avatar   string `gorm:"type:" json:"avatar"`
+	Username string `gorm:"type:varchar(20);not null " json:"username" validate:"required,min=4,max=12" label:"用户名"`
+	Password string `gorm:"type:varchar(500);not null" json:"password" validate:"required,min=6,max=120" label:"密码"`
+	Role     int    `gorm:"type:int;DEFAULT:2" json:"role" validate:"required,gte=2" label:"角色码"`
+}
+
+// CheckUser 查询用户是否存在
+func CheckUser(name string) (code int) {
+	var users User
+	db.Select("id").Where("username = ?", name).First(&users)
+	if users.ID > 0 {
+		return errmsg.ErrorUsernameUsed
+	}
+	return errmsg.SUCCESS
+}
+
+// CreateUser 新增用户
+func CreateUser(data *User) int {
+	// 密码再写入数据库之前进行加密
+	data.Password = ScryptPw(data.Password)
+	err := db.Create(&data).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
+
+// GetUsers 查询用户列表
+func GetUsers(username string, pageSize int, pageNum int) ([]User, int64) {
+	var users []User
+	err := db.Select("id,username,role").Where(
+		"username LIKE ?", username+"%",
+	).Limit(pageSize).Offset((pageNum - 1) * pageSize).Find(&users).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, 0
+	}
+	return users, 0
+}
+
+//  编辑用户
+
+// 删除用户
+
+// ScryptPw 密码加密
+func ScryptPw(password string) string {
+	const KeyLen = 10
+	salt := make([]byte, 8)
+	salt = []byte{12, 32, 4, 6, 66, 22, 222, 11}
+	HashPw, err := scrypt.Key([]byte(password), salt, 16384, 8, 1, KeyLen)
+	if err != nil {
+		log.Fatal(err)
+	}
+	Fpw := base64.StdEncoding.EncodeToString(HashPw) //最后转成字符串
+	return Fpw
 }

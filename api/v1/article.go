b8731b30dc6945ee854bfcad76d848f3350288bc
井 <jeckwell@126.com>
2023-03-18 20:56:50 +0800

    文章内容的增、删、改

--- api/v1/article.go
+++ api/v1/article.go
@@ -1,11 +1,62 @@
 package v1
 
-// 添加文章
+import (
+	"ginweb/model"
+	"ginweb/utils/errmsg"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
 
-// 查询单个文章
+// AddArticle 添加文章
 
-// 查询文章列表
+func AddArticle(c *gin.Context) {
+	var data *model.Article
+	_ = c.ShouldBindJSON(&data)
+	code := model.CreateArt(data)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
 
-// 编辑文章
+// 查询分类下的所有文章
 
-// 删除文章
+// 查询单个文章信息
+
+// GetArt 查询文章列表
+func GetArt(c *gin.Context) {
+	size, _ := strconv.Atoi(c.Query("pageSize"))
+	num, _ := strconv.Atoi(c.Query("pageNum"))
+	code = errmsg.SUCCESS
+	data, _ := model.GetCategory(size, num)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
+
+// EditArt 修改文章
+func EditArt(c *gin.Context) {
+	var data *model.Article
+	id, _ := strconv.Atoi(c.Param("id"))
+	_ = c.ShouldBindJSON(&data)
+	code := model.EditArt(id, data)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
+
+}
+
+// DeleteArt 删除文章
+func DeleteArt(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	code := model.DeleteArt(id)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
--- model/Article.go
+++ model/Article.go
@@ -1,11 +1,12 @@
 package model
 
 import (
+	"ginweb/utils/errmsg"
 	"gorm.io/gorm"
 )
 
 type Article struct {
-	Category Category `gorm:"foreignkey:Id"`
+	Category Category `gorm:"foreignkey:Cid"`
 	gorm.Model
 	Title        string `gorm:"type:varchar(100);not null" json:"title"`
 	Cid          int    `gorm:"type:int;not null" json:"cid"`
@@ -15,3 +16,57 @@ type Article struct {
 	CommentCount int    `gorm:"type:int;not null;default:0" json:"comment_count"`
 	ReadCount    int    `gorm:"type:int;not null;default:0" json:"read_count"`
 }
+
+// CreateArt 新增文章
+
+func CreateArt(data *Article) int {
+	err := db.Create(&data).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
+
+//  查询分类的所有文章
+
+// 查询单个文章
+
+// 查询文章列表
+
+func GetArt(pageSize int, pageNum int) ([]Article, int64) {
+	var cate []Article
+	err := db.Limit(pageSize).Offset((pageNum - 1) * pageSize).Find(&cate).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, 0
+	}
+	return cate, 0
+}
+
+// EditArt  编辑文章
+func EditArt(id int, data *Article) int {
+	var art Article
+	var maps = make(map[string]interface{})
+	maps["title"] = data.Title
+	maps["cid"] = data.Cid
+	maps["desc"] = data.Desc
+	maps["content"] = data.Content
+	maps["img"] = data.Img
+	err = db.Model(&art).Where("id = ? ", id).Updates(maps).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+
+}
+
+// DeleteArt 删除文章
+func DeleteArt(id int) int {
+	var art Article
+	err = db.Where("id = ?", id).Delete(&art).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
+
+// todo 查询分类下的所有文章

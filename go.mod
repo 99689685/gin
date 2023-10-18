a5f021c063fd9aa93b91ad8c8119e90bad3d656b
井井One <jeckwell@126.com>
2023-10-18 16:29:58 +0800

    ORC识别测试

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.9.0
 	github.com/goccy/go-json v0.10.0
+	github.com/otiai10/gosseract/v2 v2.4.1
+	gocv.io/x/gocv v0.35.0
 	golang.org/x/crypto v0.6.0
 	gopkg.in/ini.v1 v1.67.0
 	gorm.io/driver/mysql v1.4.7
@@ -29,9 +31,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
-	github.com/tidwall/gjson v1.17.0 // indirect
-	github.com/tidwall/match v1.1.1 // indirect
-	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.10 // indirect
 	golang.org/x/arch v0.2.0 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,88 @@
 package main
 
 import (
-	"ginweb/model"
-	"ginweb/routers"
+	"fmt"
+	"gocv.io/x/gocv"
+	"image"
 )
 
 func main() {
-	// 引用数据库
-	model.InitDb()
-	// 路由地址
-	routers.InitRouter()
+	//// 引用数据库
+	//model.InitDb()
+	//// 路由地址
+	//routers.InitRouter()
+	// 加载身份证图像
+	imagePath := "snipaste.png" // 身份证图像路径
+
+	// 读取身份证图像
+	img := gocv.IMRead(imagePath, gocv.IMReadColor)
+	if img.Empty() {
+		fmt.Println("Failed to read image")
+		return
+	}
+	defer img.Close()
+
+	// 进行图像处理，如裁剪、灰度化、二值化等操作
+
+	// 裁剪出姓名和身份证号码区域的图像
+	nameImg := img.Region(nameRegion())         // 自定义函数，根据实际情况裁剪出姓名区域
+	idNumberImg := img.Region(idNumberRegion()) // 自定义函数，根据实际情况裁剪出身份证号码区域
+	defer nameImg.Close()
+	defer idNumberImg.Close()
+
+	// 转换为灰度图像
+	grayName := gocv.NewMat()
+	gocv.CvtColor(nameImg, &grayName, gocv.ColorBGRToGray)
+	defer grayName.Close()
+
+	grayIDNumber := gocv.NewMat()
+	gocv.CvtColor(idNumberImg, &grayIDNumber, gocv.ColorBGRToGray)
+	// 进行图像二值化处理
+	binaryName := gocv.NewMat()
+	gocv.Threshold(grayName, &binaryName, 0, 255, gocv.ThresholdBinaryInv|gocv.ThresholdOtsu)
+	defer binaryName.Close()
+
+	binaryIDNumber := gocv.NewMat()
+	gocv.Threshold(grayIDNumber, &binaryIDNumber, 0, 255, gocv.ThresholdBinaryInv|gocv.ThresholdOtsu)
+	defer binaryIDNumber.Close()
+
+	// 保存二值化后的图像用于文字识别
+	gocv.IMWrite("name_binary.jpg", binaryName)
+	gocv.IMWrite("id_number_binary.jpg", binaryIDNumber)
+
+	// 使用Tesseract OCR进行文字识别
+	nameText := ocr("name_binary.jpg")
+	idNumberText := ocr("id_number_binary.jpg")
+
+	fmt.Println("姓名:", nameText)
+	fmt.Println("身份证号码:", idNumberText)
+
+}
+
+// 自定义函数，根据实际情况裁剪出姓名区域
+func nameRegion() image.Rectangle {
+	// TODO: 根据实际情况修改裁剪区域的坐标和尺寸
+	x := 100
+	y := 200
+	width := 300
+	height := 50
+
+	return image.Rect(x, y, x+width, y+height)
+}
+
+// 自定义函数，根据实际情况裁剪出身份证号码区域
+func idNumberRegion() image.Rectangle {
+	// TODO: 根据实际情况修改裁剪区域的坐标和尺寸
+	x := 100
+	y := 300
+	width := 300
+	height := 50
+
+	return image.Rect(x, y, x+width, y+height)
+}
+
+// 使用Tesseract OCR进行文字识别
+func ocr(imagePath string) string {
+	// TODO: 调用Tesseract OCR库进行文字识别并返回结果
+	return ""
 }
